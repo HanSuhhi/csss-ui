bf404d40a1362da1846cebdb7d6c8a0deeb8e549
csssUI <52781024+HanSuhhi@users.noreply.github.com>
2022-12-06 11:52:40 +0000

    website deploy

---
+++ assets/chunks/HeaderFooter.e4549f9a.js
@@ -0,0 +1,1 @@
+import{_ as a,o as r,k as s,l as e,r as _,b as o}from"../app.4c81a726.js";const n={};function c(d,f){const t=_("CLayout");return r(),s(t,{class:"header-footer"},{header:e(()=>[o("header")]),aside:e(()=>[o("aside")]),footer:e(()=>[o("footer")]),default:e(()=>[o(" main ")]),_:1})}const i=a(n,[["render",c]]);export{i as default};
---
+++ assets/chunks/LeftRight.101150df.js
@@ -0,0 +1,1 @@
+import{_ as a,o as s,k as _,l as e,r as n,b as t}from"../app.4c81a726.js";const c={};function r(i,f){const o=n("CLayout");return s(),_(o,{class:"left-right"},{aside:e(()=>[t("aside")]),default:e(()=>[t(" main ")]),_:1})}const d=a(c,[["render",r]]);export{d as default};
