ba5634baf230b4e77a5d86cf5f083b64548af3b3
Don <l_98b@outlook.com>
2022-12-10 13:38:03 +0800

    fix: some bug with using csss

--- src/client/App.vue
+++ src/client/App.vue
@@ -4,30 +4,28 @@ import { useCsssLayout } from "../packages/layout/composables/csssLayout";
 const { COMP: Layout } = useCsssLayout({
   setAsideWidthSize: ["small"],
   setStyleValue: [{ "--header-height": 10 }],
+  setFooterHeightSize: ["large"],
   setLayoutType: ["header-aside"],
 });
 </script>
 
 <template>
   <CLayout ref="Layout">
-    <p>main</p>
-    <p>main</p>
-    <p>main</p>
+    <CTabs>
+      <template #list="{ num }">
+        <p>tab 1{{ num }}</p>
+        <p>tab 2</p>
+      </template>
+      <template #panel0>
+        <p>1</p>
+      </template>
+      <template #panel1>
+        <h1>2</h1>
+      </template>
+    </CTabs>
 
-    <template #header>
-      <CTabs>
-        <template #list="{ num }">
-          <p>tab 1{{ num }}</p>
-          <p>tab 2</p>
-        </template>
-        <template #panel0>
-          <p>1</p>
-        </template>
-        <template #panel1>
-          <h1>2</h1>
-        </template>
-      </CTabs>
-    </template>
+    <template #header> 1 </template>
+
     <template #aside />
     <template #footer>footer</template>
   </CLayout>
