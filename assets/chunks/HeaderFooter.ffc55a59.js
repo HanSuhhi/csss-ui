bbb76764026ecccd72752f90ac94bd8d87577c57
csssUI <52781024+HanSuhhi@users.noreply.github.com>
2022-12-18 15:14:02 +0000

    website deploy

--- assets/chunks/HeaderFooter.ffc55a59.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as r,k as s,l as e,r as _,b as o}from"../app.15310426.js";const n={};function c(d,f){const t=_("CLayout");return r(),s(t,{class:"header-footer"},{header:e(()=>[o("header")]),aside:e(()=>[o("aside")]),footer:e(()=>[o("footer")]),default:e(()=>[o(" main ")]),_:1})}const i=a(n,[["render",c]]);export{i as default};
